c51dd3b76ebf14da9b74de35812f1da59b112074
Suresh Ramaswamy <suresh_ramaswamy_65@yahoo.com>
2018-04-28 14:57:31 -0400

    Graduate Credit Assignment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var url = require('url');
 
-// Import express-session, which will manage user sessions for your website, and oidc-middleware, which will handle all of the OIDC implementation details for your website
+// Import express-session, which will manage user sessions for this website, and oidc-middleware, which will handle all of the OIDC implementation details for this website
 
 const session = require("express-session");
 const ExpressOIDC = require("@okta/oidc-middleware").ExpressOIDC;
@@ -21,6 +21,18 @@ app.locals.bookreviews = [];
 var bookreviews = require('./routes/bookreviews');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+//Instantiate the OIDC object that is responsible for the call flowsThe following information is required for this object.
+
+// issuer: this should be your Org URL value (that you wrote down earlier) with /oauth2/default appended. This is the OAuth2 endpoint that’s used for handling authorization.
+
+// client_id/client_secret: these values are what you wrote down earlier after creating your Okta Application. They can be found in your Application settings in Okta.
+
+// redirect_uri: this setting tells Okta where to redirect the user after they’ve signed in. This value should stay the same as it is listed above, as this library will handle that route for you automatically.
+
+// routes.callback.default_redirect: this option tells Okta where to redirect a user once they’ve been signed into your website. In this case, you’ll want to redirect them to the dashboard page.
+
+// scope: the OpenID Connect protocol has a lot of standard scopes that determine what data about your user is returned to you once the user has been signed in. The values here provide basic user information for this website. 
 
 let oidc = new ExpressOIDC({
   issuer: "https://dev-766675.oktapreview.com/oauth2/default",
@@ -48,7 +60,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/logout", (req, res) => {
-  console.log("XXXXX LOG OUT CALLED");
+  console.log("LOG OUT CALLED");
   req.logout();
   console.log(req.userinfo);
   res.redirect("/");
--- readme.md
+++ readme.md
@@ -1,3 +1,14 @@
-# Assignment #3 - ExpressJS, Routing and Templates
+// Graduate Credit Assignment for CSCIE-31
 
-This assignment builds a book review website where you can review any book.  The input form takes the title of the book, the author of the book, reviewer's name and the review as input.  When the user clicks on the submit button, this application will display a page containing all the reviews of books thus far.
+
+This project incorporates security enhancements to my book reviews website that I created via Assignment 3.   The security enhancements make use of OpenID Connect protocol.  OpenID Connect is an open standard protocol for authenticating an user via a trusted Identity Provider (IDP) and for an application to get user’s profile from the trusted IDP based on user’s authorization to the IDP.   OpenID Connect is an authentication feature built on top of the OAuth 2.0 which is the authorization standard.
+
+
+My book reviews website has the following fields:
+Title
+Author
+Reviewer
+Review
+
+This project also auto fills the review’s field after the user signs into the website to review a book.
+ 
